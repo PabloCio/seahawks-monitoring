cca0a9490f003bd705868b844bfe95adc6b66fc9
NicolasPersonnaz <117669784+NicolasPersonnaz@users.noreply.github.com>
2025-02-03 11:12:52 +0100

    scan windows

---
+++ modules/wan.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def test_latence():
+    try:
+        result = subprocess.run(["ping", "-n", "1", "8.8.8.8"], capture_output=True, text=True)
+        #print(result.stdout)  # Affiche la sortie du ping
+        for line in result.stdout.split("\n"):
+            if "temps" in line or "time=" in line:
+                return float(line.split("time=" if "time=" in line else "temps=")[1].split(" ")[0])
+    except Exception as e:
+        print(f"Erreur : {e}")
+        return None
+
+latency = test_latence()
+print(f"Latence vers Google : {latency} ms" if latency else "Impossible de mesurer la latence.")
