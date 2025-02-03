d03a5ef9742661d5e14376fdf1614f531da7fe0d
NicolasPersonnaz <117669784+NicolasPersonnaz@users.noreply.github.com>
2025-02-03 15:08:04 +0100

    json v1

--- modules/json.py
+++ modules/json.py
@@ -1,5 +1,17 @@
-# Export en JSON
 import json
 
-def export_json():
-    x = 0
+data = [
+
+    {
+    "Hostname": "nico",
+    "ip": "192.168.100.10",
+    "port": 80,
+    },
+    {
+    "Hostname": "nico",
+    "ip": "192.168.100.10",
+    "port": 80
+}
+]
+with open("D:/cours/B3/mspr/scan.json", "w") as f:
+    json.dump(data, f)
