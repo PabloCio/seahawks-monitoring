5e04dc69b9eff8082475024eead00863bad87f3c
PabloCio <93977962+PabloCio@users.noreply.github.com>
2025-02-04 22:34:01 +0100

    Conf ui_main.py / ajout titre

--- ui/ui_main.py
+++ ui/ui_main.py
@@ -1,13 +1,18 @@
 import tkinter as tk
 
 class MainApp(tk.Tk):
-    """Application principale Tkinter."""
+    # Fenetere globale avec Tkinter
     def __init__(self):
         super().__init__()
-        self.title("Seahawks Monitoring | Harvester _ Version DEV")
+        self.title(" Harvester | client - Version DEV")
         self.geometry("700x600")
+        self.configure(bg="#f0f0f0") #Définie la couleur du fond
+        self.resizable(width=False, height=False) #Interdit le redimensionnement de la fenêtre
 
         # Ajout des widgets
+        titre = tk.Label(self, text="Seahawks Harvester", bg="#f0f0f0", font=("Arial", 16, "bold"))
+        titre.pack(pady=20, padx=20)
+
 
 
 # Pour tester directement cette fenêtre en lançant ui_main.py
