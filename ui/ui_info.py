92cd55b8be1f67a8b5ab699a0e349b75a7162ecf
PabloCio <93977962+PabloCio@users.noreply.github.com>
2025-02-12 20:30:25 +0100

    merge nico

--- ui/ui_info.py
+++ ui/ui_info.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
-from features.system_info import get_hostname, get_local_ip
+
+from features.system_info import get_hostname, get_local_ip, get_wan_latency, get_connected_devices_count
 
 class InfoFrame(tk.Frame):
     def __init__(self, parent):
@@ -10,6 +11,8 @@ class InfoFrame(tk.Frame):
         # Récuparation des infos système
         hostname = get_hostname()
         local_ip = get_local_ip()
+        latency = get_wan_latency()
+        count = get_connected_devices_count(f"{local_ip}/24")
 
         # Création des Labels
         self.hostname_label = tk.Label(self, text=f"Hostname : {hostname}", bg="grey", fg="black", font=("Arial", 12))
@@ -17,3 +20,9 @@ class InfoFrame(tk.Frame):
 
         self.local_ip_label = tk.Label(self, text=f"Adresse IP : {local_ip}", bg="grey", fg="black", font=("Arial", 12))
         self.local_ip_label.pack()
+
+        self.latency_label = tk.Label(self, text=f"Latence : {latency}", bg="grey", fg="black", font=("Arial", 12))
+        self.latency_label.pack()
+
+        self.count_label = tk.Label(self, text=f"Nombre de machines : {count}", bg="grey", fg="black", font=("Arial", 12))
+        self.count_label.pack()
