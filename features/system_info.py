e009f98d0958f196a84e1f09dc405bc8c6a128d8
pablo <93977962+PabloCio@users.noreply.github.com>
2025-03-05 19:53:36 +0100

    Modification option -C WAN for Linux

--- features/system_info.py
+++ features/system_info.py
@@ -69,7 +69,7 @@ def get_cidr():
 
 def get_wan_latency():
     try:
-        result = subprocess.run(["ping", "-n", "1", "8.8.8.8"], capture_output=True, text=True) # option -c pour Debian et -n pour Windows
+        result = subprocess.run(["ping", "-c", "1", "8.8.8.8"], capture_output=True, text=True) # option -c pour Debian et -n pour Windows
         #print(result.stdout)  # Affiche la sortie du ping
         for line in result.stdout.split("\n"):
             if "temps" in line or "time=" in line:
